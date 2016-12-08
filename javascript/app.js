3c4b9e8cf3a061c7fcac0ca30ea5cd21d60cde81
20020685 <johngreer1978@gmail.com>
2016-12-08 11:58:17 +0000

    finish from here

--- javascript/app.js
+++ javascript/app.js
@@ -3,3 +3,20 @@
  */
 'use strict';
 var learnjavascript={};
+learnjavascript.questionView=function(){
+    return $('<div class="question-view">').text('Arriving Soon!!!');
+};
+
+learnjavascript.showView = function (hash){
+    var routes={
+        '#question-1': learnjavascript.questionView};
+
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('view-container').empty().append(viewFn());
+    }
+
+};
+
+
+
