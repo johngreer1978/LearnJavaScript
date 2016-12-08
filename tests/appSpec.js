b3ba6de3f00a9dc10110df2ff278b723b1f97e1c
20020685 <johngreer1978@gmail.com>
2016-12-08 11:55:52 +0000

    finish from here

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -6,4 +6,8 @@ describe('LearnJavascript', function(){
         learnjavascript.showView('#question-1');
         expect($('.view-container .question-view').length).toEqual(1);
     });
+    it ('shows the landing page view when their is not a hash', removeFunctionWidthId(){
+        learnjavascript.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    };
 });
